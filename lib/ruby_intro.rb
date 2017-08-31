de7876e28f88e5cb9a5c544f966229501342f2eb
Derek Devera <ddevera@g.clemson.edu>
2017-08-31 02:38:50 +0000

    Added comments

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,21 +2,19 @@
   Derek Devera
   CSCI 602 - hw-ruby-intro
 =end
-# Part 1
 
+# Part 1
 def sum arr
-  total = 0
-  arr.each { |num| total += num  }
-  total
+  arr.inject(0) { |total, a| total += a }
 end
 
 def max_2_sum arr
-  if arr.length > 2
+  if arr.length > 2 #sum of top 2
     array = arr.sort{|x, y| y <=> x}
     return array[0] + array[1]
-  elsif arr.length == 1
+  elsif arr.length == 1 #only one
     return arr[0]
-  else
+  else #empty array
     0
   end
 end
@@ -24,42 +22,40 @@ end
 def sum_to_n? arr, n
   hash = Hash.new(0)
   arr.each do |val|
-    if hash.key? val
+    if hash.key? val #value = n, no need to test
       return true
-    else
+    else #test new value (n-value) on loop back
       hash[n - val] = val
     end
-  end
+  end #couldn't find a match
   false
 end
 
 # Part 2
-
 def hello name
   "Hello, #{name}"
 end
 
-def starts_with_consonant? s
+def starts_with_consonant? s #no vowels, numbers, special chars
   s =~ /^[^aeiou\d\W](.*)/i ? true : false
 end
 
-def binary_multiple_of_4? s
+def binary_multiple_of_4? s #4 is 0d100
   s =~ /(^[10]*00$)|^0$/ ? true : false
 end
 
 # Part 3
-
 class BookInStock
-  attr_accessor :isbn, :price
+  attr_accessor :isbn, :price #thx ruby
   
   def initialize isbn, price
-    raise ArgumentError if isbn == "" || price <= 0
+    raise ArgumentError if isbn == "" || price <= 0 #not 0 or negative
     @isbn = isbn
     @price = price
   end
 
   def price_as_string
-    format("$%.2f", @price)
+    format("$%.2f", @price) #display $ with 2 decimal places
   end
   
 end
